e6e9a338d22fc1ea10d889a24f0a0aee166c9aa0
Wilko Quak <w.quak@geonovum.nl>
2023-06-23 12:30:14 +0200

    publicatie versie 4.0

--- config.js
+++ config.js
@@ -40,10 +40,10 @@ var respecConfig =
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "GN-BASIS",             // Basis Document
-  specStatus: "GN-WV",                  // Werk Versie
+  //specStatus: "GN-WV",                  // Werk Versie
   //specStatus: "GN-CV",                // Consultatie Versie
   //specStatus: "GN-VV",                // Vaststellings Versie
-  //specStatus: "GN-DEF",               // Definitieve Versie
+  specStatus: "GN-DEF",               // Definitieve Versie
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                   // Norm
   //specType: "ST",                       // Standaard
@@ -56,7 +56,7 @@ var respecConfig =
   format: "markdown",                 // altijd "markdown" - JvG W3C aangepast, op 1 plek markdown aangeven    
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-06-06",  	    // Format is "YYYY-MM-DD"
+  publishDate: "2023-06-22",  	    // Format is "YYYY-MM-DD"
   //-- Repositorynaam op GitHub -------------------------------------------------------
   github: "https://github.com/geonovum/raamwerk-geostandaarden",
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
